5d21e5e1a7606587f12d7b119f14f9ce045a001d
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2021-12-01 11:27:13 -0300

    Funçao getCountryData finalizada

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 //MÉTODO XMLHTTPREQUEST -----------------------------------------------------------
+const getCountryData = function (country) {
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.com/v3.1/name/${country}}`);
+    request.send();
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/name/brazil}");
-request.send();
+    request.addEventListener("load", () => {
+        const [dados] = JSON.parse(request.responseText);
+        console.log(dados);
 
-request.addEventListener("load", () => {
-    console.log(JSON.parse(request.responseText));
-});
+        const html = `
+        <article class="country">
+            <img class="country__img" src="${dados.flags.svg}" />
+            <div class="country__data">
+                <h3 class="country__name">${dados.name.common}</h3>
+                <h4 class="country__region">${dados.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                    +dados.population / 1000000
+                ).toFixed(1)}M people</p>
+            </div>
+        </article>`;
+        countriesContainer.insertAdjacentHTML("beforeend", html);
+        countriesContainer.style.opacity = 1;
+    });
+};
+
+getCountryData("denmark");
+getCountryData("norway");
+getCountryData("sweden");
