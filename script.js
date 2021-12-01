686e3bb4d28722e2e8cad4fc3c989457145b0292
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2021-12-01 10:38:21 -0300

    Mostra response  em JSON

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+
+//MÉTODO XMLHTTPREQUEST -----------------------------------------------------------
+
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/name/brazil}");
+request.send();
+
+request.addEventListener("load", () => {
+    console.log(JSON.parse(request.responseText));
+});
