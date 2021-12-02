993d059cb88d3a4f2fb6993847a3fb13a7822677
Pedro Marchetti Casteli <pedro.casteli@zaffarinet.com.br>
2021-12-02 09:35:18 -0300

    Botao de erro

--- script.js
+++ script.js
@@ -3,6 +3,27 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+const renderCOuntry = (dados, className = "") => {
+    const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${dados.flags.svg}" />
+            <div class="country__data">
+                <h3 class="country__name">${dados.name.common}</h3>
+                <h4 class="country__region">${dados.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                    +dados.population / 1000000
+                ).toFixed(1)}M people</p>
+            </div>
+        </article>`;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+    countriesContainer.insertAdjacentText("beforeend", msg);
+    // countriesContainer.style.opacity = 1;
+};
+
 //MÉTODO XMLHTTPREQUEST -----------------------------------------------------------
 // const getCountryData = function (country) {
 //     const request = new XMLHttpRequest();
@@ -28,22 +49,6 @@ const countriesContainer = document.querySelector(".countries");
 //         countriesContainer.style.opacity = 1;
 //     });
 // };
-
-const renderCOuntry = (dados, className = "") => {
-    const html = `
-        <article class="country ${className}">
-            <img class="country__img" src="${dados.flags.svg}" />
-            <div class="country__data">
-                <h3 class="country__name">${dados.name.common}</h3>
-                <h4 class="country__region">${dados.region}</h4>
-                <p class="country__row"><span>👫</span>${(
-                    +dados.population / 1000000
-                ).toFixed(1)}M people</p>
-            </div>
-        </article>`;
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-};
 
 // const getCountryAndNeighbour = function (country) {
 //     //AJAX call country 1
@@ -109,7 +114,16 @@ const getCountryData = function (country) {
             return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
         })
         .then((response) => response.json())
-        .then((dado) => renderCOuntry(dado[0], "neighbour"));
+        .then((dado) => renderCOuntry(dado[0], "neighbour"))
+        .catch((err) => {
+            console.log(`${err} <============>`);
+            renderError(`Something went wrong: ${err.message}. Try again!`);
+        })
+        .finally(() => {
+            countriesContainer.style.opacity = 1;
+        });
 };
 
-getCountryData("brazil");
+btn.addEventListener("click", function () {
+    getCountryData("brazil");
+});
